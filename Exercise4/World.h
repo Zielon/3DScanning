f793fec3b45f7181c5c52ebc2be5c70117e11582
Wojciech Zielonka <g36@wp.pl>
2018-11-27 00:20:53 +0100

    Building a cost function in progress

--- Exercise4/World.h
+++ Exercise4/World.h
@@ -281,7 +281,6 @@ public:
 		cv::Mat img_matches;
 		cv::drawMatches(rgb[0], keypoints[0], rgb[2], keypoints[2], matches[make_key(0, 2)], img_matches);
 		cv::imshow("Good Matches", img_matches);
-		cv::waitKey();
 	}
 
 	void bootstrap() {
@@ -295,16 +294,16 @@ public:
 		int counter = 0;
 		for (int i = 0; i < N_FRAMES; i++) {
 			for (int j = 0; j < N_FRAMES; j++) {
-				std::vector<cv::DMatch> matches_filtered; // = ...
+				std::vector<cv::DMatch> matches_filtered = matches[make_key(i, j)];
 				for (auto & m : matches_filtered) {
-					cv::Point2i kp0; // = ...
-					cv::Point2i kp1; // = ...
+					cv::Point2i kp0 = keypoints[i].data()->pt;
+					cv::Point2i kp1 = keypoints[j].data()->pt;
 					float d = depth[i].at<float>(kp0.y, kp0.x);
 					if (d == 0)
 						continue;
-					// CostFunctor *ref = new CostFunctor(?, ?, {?, ?}, {?, ?}, d, poses.col(0).data(), &K);
-					// ceres::CostFunction* cost_function = new ceres::AutoDiffCostFunction<CostFunctor, ?, 6*?>(ref);
-					// problem.AddResidualBlock(cost_function, NULL, params.data());
+					CostFunctor *ref = new CostFunctor(i, j, {kp0.x, kp0.y}, {kp1.x, kp1.y}, d, poses.col(0).data(), &K);
+					ceres::CostFunction* cost_function = new ceres::AutoDiffCostFunction<CostFunctor, 6, 6 * (N_FRAMES - 1)>(ref);
+					problem.AddResidualBlock(cost_function, NULL, params.data());
 					counter++;
 				}
 			}
