454f614933a56be0fd04601bae89fd7c40566917
Wojciech Zielonka <g36@wp.pl>
2018-12-11 18:03:25 +0100

    Skeleton start

---
+++ Project/ICP.h
@@ -0,0 +1,9 @@
+#ifndef PROJECT_ICP_H
+#define PROJECT_ICP_H
+
+class ICP{
+public:
+private:
+};
+
+#endif //PROJECT_ICP_H
---
+++ Project/Tracker.h
@@ -0,0 +1,10 @@
+#ifndef PROJECT_TRACKER_H
+#define PROJECT_TRACKER_H
+
+class Tracker{
+public:
+    void computerCameraPose(const float **image, const float *pose, int width, int height);
+private:
+};
+
+#endif //PROJECT_TRACKER_H
