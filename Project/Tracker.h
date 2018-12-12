8cc4b19bf2e79799072ca66e9f3b10ed78e061b3
Wojciech Zielonka <g36@wp.pl>
2018-12-12 10:39:03 +0100

    Cmake changes

--- Project/Tracker.h
+++ Project/Tracker.h
@@ -1,6 +1,10 @@
 #ifndef PROJECT_TRACKER_H
 #define PROJECT_TRACKER_H
 
+#include <iostream>
+
+using namespace std;
+
 class Tracker{
 public:
     void computerCameraPose(const float **image, const float *pose, int width, int height);
