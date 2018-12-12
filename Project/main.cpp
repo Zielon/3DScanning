3fde7aa3140d034777b70da878226f5040d68e0d
Wojciech Zielonka <g36@wp.pl>
2018-12-12 10:29:52 +0100

    Tracker

--- Project/main.cpp
+++ Project/main.cpp
@@ -7,9 +7,13 @@
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
 
+#include "Tracker.h"
+
 int main() {
 
-	//
+	Tracker tracker;
+
+	tracker.computerCameraPose(nullptr, nullptr, 0, 0);
 
 	return 0;
 }
