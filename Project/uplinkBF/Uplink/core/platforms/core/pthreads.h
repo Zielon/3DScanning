d99d82d560f0ba5f8b886f14f2421742c68d6255
Radner <radner@in.tum.de>
2018-12-23 18:12:51 +0100

    excluded some files changed videostreamreader name to ...Base

--- Project/uplinkBF/Uplink/core/platforms/core/pthreads.h
+++
@@ -1,7 +0,0 @@
-# pragma once
-
-# include <pthread.h>
-
-# define UPLINK_THREAD    pthread_t
-# define UPLINK_MUTEX     pthread_mutex_t
-# define UPLINK_CONDITION pthread_cond_t
--- Project/uplinkBF/Uplink/core/platforms/core/windows-threads.h
+++
@@ -1,7 +0,0 @@
-# pragma once
-
-# include "./win32-api.h"
-
-# define UPLINK_THREAD    HANDLE
-# define UPLINK_MUTEX     CRITICAL_SECTION
-# define UPLINK_CONDITION CONDITION_VARIABLE
