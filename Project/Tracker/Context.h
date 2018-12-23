7eef08cf6abc26ed1951a1ee2d484bb2b479f80b
Radner <radner@in.tum.de>
2018-12-23 18:23:56 +0100

    super elegant solution to the abs path problem

--- Project/Tracker/Context.h
+++ Project/Tracker/Context.h
@@ -4,7 +4,12 @@
 #include "Tracker.h"
 #include "VideoStreamReader.h"
 
-const std::string DATASET_DIR = "D:/user/desktop/3dscanning/assets/rgbd_dataset_freiburg2_xyz/";
+
+#ifdef IDK_HOW_TO_NOT_USE_ABS_PATH_IN_THIS_SITUATION
+	const std::string DATASET_DIR = "D:/user/desktop/3dscanning/assets/rgbd_dataset_freiburg2_xyz/";
+#else
+	const std::string DATASET_DIR = "SETUP YOUR OWN DATASET PATH IN Context.h";
+#endif
 const bool ENFORCE_REALTIME = true;
 
 
