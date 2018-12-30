1ee3b10f30033e4d27f8792e0c5c4c8fdcf75350
Radner <radner@in.tum.de>
2018-12-30 18:14:18 +0100

    forgot to remove my dataset path

--- Project/Tracker/data-stream/headers/Context.h
+++ Project/Tracker/data-stream/headers/Context.h
@@ -7,7 +7,7 @@
 /*i.e.:
 D:\\user\\desktop\\3dscanning\\assets\\rgbd_dataset_freiburg2_xyz\\  
 */
-const std::string DATASET_DIR = "D:\\user\\desktop\\3dscanning\\assets\\rgbd_dataset_freiburg2_xyz\\";
+const std::string DATASET_DIR = "";
 const bool ENFORCE_REALTIME = true;
 
 struct Context {
