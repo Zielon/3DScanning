d11ad49993dce3497166398a29c28579deb2b849
BarisYazici <barisyazici@sabanciuniv.edu>
2019-01-16 00:55:14 +0100

    delete the pointers after done

--- Project/Tracker/src/main.cpp
+++ Project/Tracker/src/main.cpp
@@ -7,13 +7,15 @@ int main(int argc, char** argv){
 	auto tests = std::vector<TestBase*>();
 
 	tests.push_back(new TrackerTest());
-	//tests.push_back(new ReconstructionTest());
-	//tests.push_back(new DataStreamTest());
+	tests.push_back(new ReconstructionTest());
+	tests.push_back(new DataStreamTest());
 
 	for (auto test : tests) {
 		test->run();
 	}
 
-
+	for (auto test : tests) {
+		SAFE_DELETE(test);
+	}
 	return 0;
 }
