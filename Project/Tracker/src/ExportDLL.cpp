22a89b7ff17ee524cf60e9216ed3b3bf95889517
Wojciech Zielonka <wojtas.zielonka@gmail.com>
2019-01-20 13:44:14 +0100

    Pose multiplication fix

--- Project/Tracker/src/ExportDLL.cpp
+++ Project/Tracker/src/ExportDLL.cpp
@@ -64,7 +64,7 @@ extern "C" __declspec(dllexport) void tracker(void* context, unsigned char* imag
 	const Matrix4f delta_pose = tracker_context->m_tracker->alignNewFrame(
 		source, tracker_context->m_tracker->m_previous_point_cloud);
 
-	tracker_context->m_tracker->m_previous_pose = tracker_context->m_tracker->m_previous_pose * delta_pose;
+	tracker_context->m_tracker->m_previous_pose = delta_pose * tracker_context->m_tracker->m_previous_pose;
 
 	memcpy(pose, tracker_context->m_tracker->m_previous_pose.data(), 16 * sizeof(float));
 
