8cfe8a24c7030bbe7f4214f89d51054de81c1aa8
Juan Raul Padron Griffe <juanraul.padron@gmail.com>
2019-01-21 19:43:32 +0100

    Tracker: Include sensor (openni2) with test

--- Project/Tracker/src/tests/headers/DataStreamTest.h
+++ Project/Tracker/src/tests/headers/DataStreamTest.h
@@ -1,13 +1,20 @@
 #pragma once
 #include "TestBase.h"
+#include "../../data-stream/headers/Xtion2StreamReader.h"
+
+#include <conio.h>
 
 class DataStreamTest :
 	public TestBase
 {
 public:
 	void run() override{
-		this->vidReadTest();
+		//this->vidReadTest();
+		this->sensorTest(true);
 	};
 
+	int wasKeyboardHit(){ return (int)_kbhit(); }
+
 	void vidReadTest();
+	int sensorTest(bool useOpenni2);
 };
