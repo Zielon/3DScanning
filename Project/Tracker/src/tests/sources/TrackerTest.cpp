1f542bf2e04b17fd3a8e0ddd2d21e8cf42e63ff5
Juan Raul Padron Griffe <juanraul.padron@gmail.com>
2019-01-29 20:01:12 +0100

    Update Camera Tracker tests

--- Project/Tracker/src/tests/sources/TrackerTest.cpp
+++ Project/Tracker/src/tests/sources/TrackerTest.cpp
@@ -3,19 +3,16 @@
 void TrackerTest::cameraPoseTest(){
 	std::cout << "START cameraPoseTest()" << std::endl;
 
-	TrackerContext* tracker_context = static_cast<TrackerContext*>(createContext(
-		DatasetManager::getCurrentPath().data()));
-
-	Matrix4f prev_trajectory;
-	Matrix4f firs_trajectory_inverse = getTrajectory(0).inverse();
+	TrackerContext* tracker_context = static_cast<TrackerContext*>(createContext(DatasetManager::getCurrentPath().data()));
 
 	//Statistics variables
 	double icp_time = 0.0;
 	float final_error = 0.0f, avg_error = 0.0f;
 	float avg_displacement_error = 0.0f, displacement_error = 0.0f;
 
-	int nIters = 700; //3000
-	Matrix4f trajectory;
+	int nIters = getIterations();//700
+	Matrix4f prev_trajectory, trajectory;
+	Matrix4f firs_trajectory_inverse = getTrajectory(0).inverse();
 
 	// for some reason when the scope of this mat is inside the loop it gets borked after alignNewFrame() is called 
 	for (int i = 0; i < nIters; i++)
@@ -36,23 +33,14 @@ void TrackerTest::cameraPoseTest(){
 			continue;
 		}
 
-		//tracker_context->m_tracker->m_previous_point_cloud->transform(tracker_context->m_tracker->m_pose);
-
-		//std::cout << "Previous Pose" << std::endl;
-		//std::cout << tracker_context->m_tracker->m_pose << std::endl;
-
 		std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
 
-		Matrix4f deltaPose = tracker_context->m_tracker->alignNewFrame(
-			source, tracker_context->m_tracker->m_previous_point_cloud);
+		Matrix4f deltaPose = tracker_context->m_tracker->alignNewFrame(tracker_context->m_tracker->m_previous_point_cloud, source);
 
 		Matrix4f pose = deltaPose * tracker_context->m_tracker->m_pose;
 
-		std::cout << "Vertices: source: " << source->getPoints().size() << " target: " << tracker_context
-		                                                                                  ->m_tracker->
-		                                                                                  m_previous_point_cloud->
-		                                                                                  getPoints().size() << std::
-			endl;
+		/*std::cout << "Vertices: source: " << source->getPoints().size() << 
+			" target: " << tracker_context->m_tracker->m_previous_point_cloud->getPoints().size() << std::endl;*/
 
 		std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
 
@@ -62,13 +50,10 @@ void TrackerTest::cameraPoseTest(){
 		// Safe the last frame reference
 		tracker_context->m_tracker->m_previous_point_cloud = source;
 		tracker_context->m_tracker->m_pose = pose;
-
-		//trackerCameraPose(pc, img, pose);
-		imshow("dllTest", rgb);
+		
+		//Check with image
+		/*imshow("dllTest", rgb);
 		cv::waitKey(1);
-
-		//Compute the inverse of the pose
-		//matPose = matPose.inverse().eval();
 
 		std::cout << "\n ------- pose: " << i << " -------- \n" << pose
 			<< "\n------------------------ " << std::endl;
@@ -77,16 +62,16 @@ void TrackerTest::cameraPoseTest(){
 			<< "\n------------------------ \n" << std::endl;
 
 		std::cout << "\n ------- Delta Pose: " << i << " -------- \n" << deltaPose
-			<< "\n------------------------ \n" << std::endl;
+			<< "\n------------------------ \n" << std::endl;*/
 
 		//std::cout << "\n ------- trajectory difference: " << i << " -------- \n" << prev_trajectory.inverse() * trajectory
 		//	<< "\n------------------------ \n" << std::endl;
 
 		prev_trajectory = trajectory;
 
-		//Error using Frobenius norm
 		//Performance metric should be Absolute Trajectory Error (ATE) https://vision.in.tum.de/data/datasets/rgbd-dataset/tools#evaluation
 
+		//Error using Frobenius norm
 		Matrix4f error = pose - trajectory;
 
 		double prev_drift = final_error;
@@ -100,15 +85,14 @@ void TrackerTest::cameraPoseTest(){
 
 		std::cout << "\n ------- Error: " << i << " -------- \n" << final_error
 			<< "\n ------- Translational Error: " << i << " -------- \n" << displacement_error
-
 			<< "\n------------------------ " << std::endl;
 	}
 
-	std::cout << "Average ICP time:  " << 1000.0 * icp_time / nIters << " ms\n";
-	std::cout << "Total ICP error:  " << 100.0 * final_error << " cm\n";
-	std::cout << "Average ICP error:  " << 100.0* avg_error / nIters << " cm\n";
-	std::cout << "Total ICP displacement error:  " << 100.0 * displacement_error << " cm\n";
-	std::cout << "Average ICP displacement error:  " << 100.0 * avg_displacement_error / nIters << " cm\n";
+	std::cout << "Average ICP time:  " << 1000.0f * icp_time / nIters << " ms\n";
+	std::cout << "Total ICP error:  " << 100.0f * final_error << " cm\n";
+	std::cout << "Average ICP error:  " << 100.0f * avg_error / nIters << " cm\n";
+	std::cout << "Total ICP displacement error:  " << 100.0f * displacement_error << " cm\n";
+	std::cout << "Average ICP displacement error:  " << 100.0f * avg_displacement_error / nIters << " cm\n";
 
 	std::cin.get();
 }
